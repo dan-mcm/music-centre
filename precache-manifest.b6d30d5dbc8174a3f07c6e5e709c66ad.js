7ca100bb16b9aade2a963c6d36fb93ed2c5d23cc
daniel40392 <daniel40392@gmail.com>
2019-11-01 21:27:25 +0000

    Updates

---
+++ precache-manifest.b6d30d5dbc8174a3f07c6e5e709c66ad.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303266ec5d4e17c0d6bc40db91c5a67d",
+    "url": "/music-centre/index.html"
+  },
+  {
+    "revision": "83b098fc88e27ad8616c",
+    "url": "/music-centre/static/css/main.7dfc7124.chunk.css"
+  },
+  {
+    "revision": "1a0c44aff91aeb4b3668",
+    "url": "/music-centre/static/js/2.3f6584b6.chunk.js"
+  },
+  {
+    "revision": "83b098fc88e27ad8616c",
+    "url": "/music-centre/static/js/main.b1f7b233.chunk.js"
+  },
+  {
+    "revision": "d18a6c779594cbc67de7",
+    "url": "/music-centre/static/js/runtime-main.d11bf3f9.js"
+  },
+  {
+    "revision": "dc318d94492b7c5df5e17cbb8231bbe4",
+    "url": "/music-centre/static/media/4-4 Emphasis.dc318d94.jpg"
+  },
+  {
+    "revision": "a098cfdfc6398b13bd45465077a788d6",
+    "url": "/music-centre/static/media/8modes.a098cfdf.png"
+  },
+  {
+    "revision": "b93a56c1b432ed91b4cf555333d5f2a4",
+    "url": "/music-centre/static/media/chord-wheel.b93a56c1.jpg"
+  },
+  {
+    "revision": "b4f84777f252b369eabe7ce99f8f7f1f",
+    "url": "/music-centre/static/media/commontime.b4f84777.jpg"
+  },
+  {
+    "revision": "b6abd8ab17d93e279de4ad78f6141d4a",
+    "url": "/music-centre/static/media/compound+simple.b6abd8ab.jpg"
+  },
+  {
+    "revision": "0c357666bf7ba455958251c65a7d54e3",
+    "url": "/music-centre/static/media/compound-signatures.0c357666.jpg"
+  },
+  {
+    "revision": "cf0a858ab27cf27375af26c1ff47f2ac",
+    "url": "/music-centre/static/media/connolly-train.cf0a858a.jpg"
+  },
+  {
+    "revision": "2e7f2a04ba2eab8826a759f92354164b",
+    "url": "/music-centre/static/media/cork-train.2e7f2a04.jpg"
+  },
+  {
+    "revision": "b5ccef125780b7780b6aaf7297713928",
+    "url": "/music-centre/static/media/dmcm-gaming.b5ccef12.jpg"
+  },
+  {
+    "revision": "dbf1255f399d1450d0eacdd585d384fc",
+    "url": "/music-centre/static/media/dmcm-suit.dbf1255f.jpg"
+  },
+  {
+    "revision": "7a75b1eea3d18a1781a92f109048fec2",
+    "url": "/music-centre/static/media/dmcm-uku.7a75b1ee.jpg"
+  },
+  {
+    "revision": "73a40e12de2702048a0138b1d6e9f3e7",
+    "url": "/music-centre/static/media/ferrycarrig-brand.73a40e12.jpg"
+  },
+  {
+    "revision": "c3aec5f717ae9b0ba26aa6cf70aab054",
+    "url": "/music-centre/static/media/ferrycarrig-upright.c3aec5f7.jpg"
+  },
+  {
+    "revision": "96d618594b3175ce6a67d3c031561e33",
+    "url": "/music-centre/static/media/heuston-train.96d61859.jpg"
+  },
+  {
+    "revision": "c91314d8f3686acd5070afb830011ed6",
+    "url": "/music-centre/static/media/intervals.c91314d8.png"
+  },
+  {
+    "revision": "6132a6f7ff987ae9b61ad3708ce206e4",
+    "url": "/music-centre/static/media/keyboard-stave.6132a6f7.jpg"
+  },
+  {
+    "revision": "96153a48dd64651b6fff8b68b37a39c2",
+    "url": "/music-centre/static/media/keysignatures.96153a48.gif"
+  },
+  {
+    "revision": "d6269c74ee22a11b07543509b3b37596",
+    "url": "/music-centre/static/media/metropole-grand-lid.d6269c74.jpg"
+  },
+  {
+    "revision": "27246624872bec779613020f6b295940",
+    "url": "/music-centre/static/media/metropole-grand.27246624.jpg"
+  },
+  {
+    "revision": "b8c6e29db688f8ae464abdcb818e3d9f",
+    "url": "/music-centre/static/media/metropole-owner.b8c6e29d.jpg"
+  },
+  {
+    "revision": "8b748ce1a951c43a080b2a2ba449a076",
+    "url": "/music-centre/static/media/powerscourt-bechstein.8b748ce1.jpg"
+  },
+  {
+    "revision": "a121bcc9d1cca8511ea1af6ae3a631c9",
+    "url": "/music-centre/static/media/powerscourt-grand.a121bcc9.jpg"
+  },
+  {
+    "revision": "53c5b59282adfbdd68b8c12465195876",
+    "url": "/music-centre/static/media/rhythm-hierarchy.53c5b592.jpg"
+  },
+  {
+    "revision": "eec84f1fb5e87b9e62c1b12fb413f06b",
+    "url": "/music-centre/static/media/stave-space.eec84f1f.jpg"
+  },
+  {
+    "revision": "ba5732fd851ccc013431a482b100df86",
+    "url": "/music-centre/static/media/treblebass.ba5732fd.jpg"
+  },
+  {
+    "revision": "82be5b5c297b404a8400e63c28b6b2c5",
+    "url": "/music-centre/static/media/vocal-rhythm.82be5b5c.jpg"
+  }
+]);
